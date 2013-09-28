c11b1161f27e7c8fc2c74d9e447035352f68b781
Sastha K L <shazcool@gmail.com>
2013-09-28 22:20:02 +0800

    fblogin and logout

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var smApp = angular.module('smApp',['ui.date','ui.bootstrap','ui.keypress','ngRoute','ngCookies', 'ngGrid']);
+var smApp = angular.module('smApp',['ui.date','ui.bootstrap','ui.keypress','ngRoute','ngCookies', 'ngResource', 'ngGrid']);
 
 //Routing
 smApp.config(function ($routeProvider) {
@@ -15,64 +15,104 @@ smApp.config(function ($routeProvider) {
             })
         .otherwise({ redirectTo: '/' });
 });
-smApp.service('Facebook', function($q, $rootScope) {
-  
-  // resolving or rejecting a promise from a third-party
-  // API such as Facebook must be
-  // performed within $apply so that watchers get
-  // notified of the change
-  resolve = function(errval, retval, deferred) {
-    $rootScope.$apply(function() {
-      if (errval) {
-        deferred.reject(errval);
-      } else {
-        retval.connected = true;
-        deferred.resolve(retval);
-      }
-    });
-  }
-    
-  return {
-    getUser: function(FB) {
-      var deferred = $q.defer();
-      FB.getLoginStatus(function(response) {
-        if (response.status == 'connected') {
-          FB.api('/me', function(response) {
-            resolve(null, response, deferred);
-          });
-        } else if (response.status == 'not_authorized') {
-          FB.login(function(response) {
-            if (response.authResponse) {
-              FB.api('/me', function(response) {
-                resolve(null, response, deferred);
-              });
-            } else {
-              resolve(response.error, null, deferred);
-            }
-          });
-        } 
+smApp.service('Facebook',function($rootScope) {
+        this.askFacebookForAuthentication = function(fail, success) {
+            FB.login(function(response) {
+            	console.log(response);					
+                if (response.authResponse) {
+                    FB.api('/me', function() { $rootScope.$apply(success) });
+                } else {
+                   $rootScope.$apply(function() {
+                     fail('User cancelled login or did not fully authorize.')
+                   });
+                }
+            });
+        }
+});
+
+smApp.service('facebook', ['$rootScope', '$window', function ($rootScope, $window) {
+
+  this.askFacebookForAuthentication = function (fail, success) {
+    FB.login(function (response) {
+      $rootScope.$apply(function () {
+        if (response.authResponse) {
+          FB.api('/me', success);
+        } else {
+          fail('User cancelled login or did not fully authorize.');
+        }
       });
-      promise = deferred.promise;
-      promise.connected = false;
-      return promise;
-    },
-    login:function(FB){
-    	var deferred = $q.defer();
-    	FB.login(function(response) {
-            if (response.authResponse) {
-              FB.api('/me', function(response) {
-                resolve(null, response, deferred);
-              });
-            } else {
-              resolve(response.error, null, deferred);
-            }
-          });
-      promise = deferred.promise;
-      promise.connected = false;
-      return promise;
+    });
+  };
+
+  this.getLoginStatus = function () {
+    FB.getLoginStatus(function (response) {
+      return response;
+    });
+  };
+
+  this.FB = $window.FB;
+
+}]);
+
+/* --------------- FB API MODELS ------------------ */
+smApp.service('FBUser', ['$log', '$rootScope', 'facebook', function ($log, $rootScope, facebook) {
+  var that = this;
+
+  this.authorized = false;
+
+  facebook.FB.Event.subscribe('auth.authResponseChange', function (response) {
+    $log.info("Event: auth.authResponseChange");
+    if (response.authResponse) {
+      if (response.status === 'connected') {
+        // User logged in and authorized
+        $log.info('User logged in and authorized');
+        $rootScope.$apply(function () {
+          that.authorized = true;
+        });
+        // DO WORK
+      } else if (response.status === 'not_authorized') {
+        // User logged in but has not authorized app
+        $log.info('User logged in');
+        $rootScope.$apply(function () {
+          that.authorized = false;
+        });
+      } else {
+        // User logged out
+        $log.info('User logged out');
+        $rootScope.$apply(function () {
+          that.authorized = false;
+        });
+      }
+    } else {
+      $log.info('No valid authResponse found, user logged out');
+      $rootScope.$apply(function () {
+        that.authorized = false;
+      });
     }
-  }; 
- });
+  });
+
+  this.login = function (success, fail) {
+    facebook.FB.login(function (response) {
+      $rootScope.$apply(function () {
+        if (response.authResponse) {
+          console.log(response);
+        } else {
+          console.log('Login unsuccessful');
+        }
+      });
+    });
+  };
+
+  this.logout = function () {
+    facebook.FB.logout(function () {
+      $rootScope.$apply(function () {
+        that.authorized = false;
+      });
+    });
+  };
+}]);
+
+
 //Home Controller
 smApp.controller('appController', function($scope, $cookies){
 
@@ -92,19 +132,24 @@ smApp.controller('appController', function($scope, $cookies){
 	
 });
 
-//Login Controller
-smApp.controller('loginController', function($scope, $cookies, Facebook){
+smApp.controller('loginController', ['$scope', 'FBUser',function($scope, FBUser) {
+  $scope.user = FBUser;
+}]);
 
-	/*Example for Cookies
-	$cookies.City = 'London';
-    $scope.cookieValue = $cookies['City'];
-    */
-    $scope.user = Facebook.login(FB);
-  	//Initializer
-	init();
-	function init(){
-		
-		
-	};
+//Login Controller
+smApp.controller('loginController1', function($scope, $cookies, $resource, Facebook){
+
+	$scope.user = {}
+    $scope.error = null;
+
+    $scope.registerWithFacebook = function() {
+        Facebook.askFacebookForAuthentication(
+        function(reason) { // fail
+            $scope.error = reason;
+        }, function(user) { // success
+            $scope.user = user
+        });
+    }
+    
 	
 });
